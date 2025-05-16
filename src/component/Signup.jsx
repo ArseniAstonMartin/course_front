fb78241d5834bae0c369e2fe41127c452df64360
ArseniAstonMartin <zarseniy280@gmail.com>
2025-05-16 15:01:24 +0300

    Потестировал фронт, всё работает

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Authorization.css';
-import AuthService from '../services/auth.service';
+import AuthService from '../service/auth.service';
 
 const Signup = () => {
   const [formData, setFormData] = useState({
